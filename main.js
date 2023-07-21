f6ce484dc2eb21d614d6cecc51d0f7abca56cff2
Caio Elias <133777500+CaioEliws@users.noreply.github.com>
2023-07-21 10:13:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+
+// Exercicio 1
+// alert("Hello word!")
+
+// Exercicio 2
+// let NumberOne = Number(prompt("Declare aqui um numéro:"))
+// let NumberTwo = Number(prompt("Declare aqui o segundo número:"))
+
+// let som = NumberOne + NumberTwo
+
+// alert("O resultado da soma dos dois números é: " + som)
+
+// Exercicio 3
+// const isNumber = () => {
+//     let value = prompt('Digite qualquer coisa: ');
+    
+//     // Verifica se o valor é um número
+//     if (!isNaN(Number(value))) {
+//       return "É um número";
+//     } else {
+//       return "Não é um número";
+//     }
+//   }
+  
+//   alert(isNumber());
+
+//Exercicio 4
+// const isString = () => {
+//     let value = prompt('Digite alguma coisa: ');
+
+//     if (isNaN(String(value))) {
+//         return "É uma string"
+//     } else {
+//         return "Não é uma string"
+//     }
+// }
+
+// alert (isString());
+
+
+// Exercicio 6
+// let NumberOne = Number(prompt("Declare aqui um numéro:"))
+// let NumberTwo = Number(prompt("Declare aqui o segundo número:"))
+
+// let sub = NumberOne - NumberTwo
+
+// alert("O resultado da subtração dos dois números é: " + sub)
+
+// Exercicio 7
+// let NumberOne = Number(prompt("Declare aqui um numéro:"))
+// let NumberTwo = Number(prompt("Declare aqui o segundo número:"))
+
+// let multi = NumberOne * NumberTwo
+
+// alert("O resultado da multiplicação dos dois números é: " + multi)
+
+//Exercicio 8
+// let NumberOne = Number(prompt("Declare aqui um numéro:"))
+// let NumberTwo = Number(prompt("Declare aqui o segundo número:"))
+
+// let div = NumberOne / NumberTwo
+
+// alert("O resultado da divisão dos dois números é: " + div)
+
+// Exercicio 9
